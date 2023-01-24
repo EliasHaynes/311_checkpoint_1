74e671d6797099035b8442374760b51ce53d3542
EliasHaynes <eliasmhaynes@gmail.com>
2023-01-24 14:59:25 -0600

    finished, added error handling

--- controllers/users.js
+++ controllers/users.js
@@ -3,12 +3,22 @@ const sampleUser = require('../data/sampleUser')
 
 module.exports = {
     list: (req,res) => {
+      if (!users) {
+        // no users, throw error
+        return res.status(404).send('No users found')
+      }
         return res.send(users)
     },
+
     show: (req,res) => {
         const grabId = req.params.id
+        if (!users[grabId -1]) {
+          // no users, throw error
+          return res.status(404).send(`User ${grabId} not found`)
+        }
         res.send(users[grabId -1])
     }, 
+
     create: (req,res) => {
         const record = {
           id: users[users.length - 1].id +1,
@@ -17,21 +27,27 @@ module.exports = {
         users.push(record)
         return res.json(record)
       },
+
       update: (req,res) => {
         const grabId = req.params.id;
+        if (!users[grabId -1]) {
+          // no users, throw error
+          res.status(404).send(`User ${grabId} not found`)
+        }
         const newBody = {
           ...sampleUser
         }
         return res.json(users[grabId -1] = newBody)
       },
+
       delete: (req,res) => {
         const grabId = req.params.id;
         const idIndex = users.findIndex(user => user === users[grabId -1])
+        if (!users[grabId -1]) {
+          // no users, throw error
+          res.status(404).send(`User ${grabId} not found`)
+        }
         users.splice(idIndex, 1)
         return res.json(users)
-        // const filteredUsers = users.filter(user => user !== users[grabId-1])
-        // return res.json(filteredUsers)
-
       }
-      
 }
